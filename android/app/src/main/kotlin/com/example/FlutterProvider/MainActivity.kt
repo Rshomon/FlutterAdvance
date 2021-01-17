b04e0db123506f7d34b96cdab622968770bc792d
Rshomon <38773298+Rshomon@users.noreply.github.com>
2021-01-17 11:08:24 +0800

    Init

---
+++ android/app/src/main/kotlin/com/example/FlutterProvider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.FlutterProvider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "FlutterProvider",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FlutterProvider"),
+      ),
+      body: Center(
+        child: Text("Hello,world"),
+      ),
+    );
+  }
+}
